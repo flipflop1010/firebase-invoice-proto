0573555e9e8cca35eb58fa0d31eb7d13c4b2ce74
DIPAYAN <dipayan.mindkorp@gmail.com>
2022-08-21 21:26:18 +0530

    invoice page added

--- src/pages/Invoice.tsx
+++ src/pages/Invoice.tsx
@@ -14,7 +14,9 @@ import {
     Container,
     CardContent,
 
-    Card
+    Card,
+    Autocomplete,
+    TextField
 } from "@mui/material"; //import the mui component 
 
 import { makeStyles } from '@mui/styles'; //import the makestyles 
@@ -37,6 +39,13 @@ const useStyles = makeStyles(() => ({
     },
 }));
 
+const customers = [
+    { customer: 'customer1' },
+    { customer: 'customer2' },
+    { customer: 'customer3' },
+    { customer: 'customer4' },
+    { customer: 'customer5' },
+];
 
 const Invoice = () => {
 
@@ -62,6 +71,10 @@ const Invoice = () => {
         setItemTotal(quantity * itemsall?.price)
     }, [itemsall, quantity])
 
+    const defaultProps = {
+        options: customers,
+        getOptionLabel: (option: any) => option.customer,
+    };
 
 
     const classes = useStyles();
@@ -79,11 +92,13 @@ const Invoice = () => {
                             Invo<span style={{ color: "orange" }}>ic</span>e
                         </h2>
 
+                        <p>Customer Invoic</p>
+
                     </Grid>
                 </Grid>
 
-                <Grid container spacing={3} sx={{ mt: 1 }}>
-                    <Grid item md={5.5} sm={6} xs={12}>
+                {/* <Grid container spacing={3} sx={{ mt: 1 }}> */}
+                {/* <Grid item md={5.5} sm={6} xs={12}>
 
                         <Card sx={{ boxShadow: "none", border: "1px solid #efefef" }}>
                             <CardContent>
@@ -104,12 +119,12 @@ const Invoice = () => {
                         </Card>
 
 
-                    </Grid>
+                    </Grid> */}
 
-                    <Grid item md={1} sm={0} xs={12}>
-                    </Grid>
-                    {/* personal Information portion start */}
-                    <Grid item md={5.5} sm={6} xs={12}>
+                {/* <Grid item md={1} sm={0} xs={12}>
+                    </Grid> */}
+                {/* personal Information portion start */}
+                {/* <Grid item md={5.5} sm={6} xs={12}>
                         <Card sx={{ boxShadow: "none", border: "1px solid #efefef" }}>
                             <CardContent>
                                 <Typography sx={{ fontSize: 14, fontWeight: "bold" }} color="text.primary" gutterBottom>
@@ -127,13 +142,47 @@ const Invoice = () => {
                             </CardContent>
                         </Card>
                     </Grid>
-                </Grid>
+                </Grid> */}
 
-                <Grid container spacing={1} sx={{ mt: 3 }}>
-                    <Grid item lg={12} md={12} xs={12} sm={12} sx={{ display: "flex", justifyContent: "right" }} >
+                <Grid container spacing={2} sx={{
+                    mt: 2, p
+                        : 2
+                }}>
+                    <Grid item lg={4} md={3} xs={12} sm={12}  >
+                        <Autocomplete
+                            fullWidth
+                            {...defaultProps}
+                            id="clear-on-escape"
+                            clearOnEscape
+                            onChange={(event, value) => selectedItem(value)}
+                            renderInput={(params) => (
+                                <TextField {...params} label="Select Customer" variant="standard" />
+                            )}
+                        />
 
-                        <Button variant="outlined" sx={{ boxShadow: "none", borderRadius: "0px" }}>Add Item</Button>
                     </Grid>
+
+                    <Grid item lg={4} md={3} xs={12} sm={12}  >
+
+                        <TextField fullWidth id="code" label="Invoice Number " variant="standard" />
+                    </Grid>
+
+                    <Grid item lg={4} md={3} xs={12} sm={12}  >
+
+                        <TextField
+                            fullWidth
+                            id="date"
+                            label="Invoice Date"
+                            type="date"
+                            defaultValue="2017-05-24"
+                            variant="standard"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+
+
                 </Grid>
                 <Box>
                     <InvoiceTable handelChangeItemQty={handelChangeItemQty} selectedItem={selectedItem} itemsall={itemsall} itemTotal={itemTotal} />
@@ -142,6 +191,11 @@ const Invoice = () => {
 
 
                 <Grid container spacing={3} sx={{ mt: 0.5 }}>
+
+                    <Grid item lg={12} md={12} xs={12} sm={12} sx={{ display: "flex", justifyContent: "right" }} >
+
+                        <Button variant="outlined" sx={{ boxShadow: "none", borderRadius: "0px" }}>Add Item</Button>
+                    </Grid>
                     <Grid item md={5.5} sm={6} xs={12}>
 
                     </Grid>
@@ -162,6 +216,7 @@ const Invoice = () => {
                 </Grid>
 
                 <Grid container spacing={1} sx={{ mt: 3 }}>
+
                     <Grid item lg={12} md={12} xs={12} sm={12} sx={{ display: "flex", justifyContent: "right" }} >
 
                         <Button variant="contained" color="success" sx={{ boxShadow: "none", borderRadius: "0px" }}>Submit</Button>
